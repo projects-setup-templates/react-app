499aab1396d70e062076fd537ca5c52bc3a16c00
Rémi Rubis <remi.rubis@yahoo.com>
2023-05-04 09:31:54 +0200

    chore(eslint): setup eslint with ts

--- app/src/ui/pages/Home/Home.test.tsx
+++ app/src/ui/pages/Home/Home.test.tsx
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react';
+
+import { Home } from './Home';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  render(<Home />);
+  const linkElement = screen.getByText(/home/i);
   expect(linkElement).toBeInTheDocument();
 });
